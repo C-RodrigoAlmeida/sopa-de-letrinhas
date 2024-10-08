e40d3dc728773ad994156bcf699cd5879d93a082
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-10-08 16:40:26 -0300

    fixed a minor bug within organizatin_detail_view that was crashing the site

--- src/organization/views/management/organization_overview.py
+++ src/organization/views/management/organization_overview.py
@@ -1,25 +1,7 @@
-from typing import Any
-from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from src.organization.models.organization import Organization
+from src.organization.views.organization.organization_detail_view import OrganizationDetailView
 
-class OrganizationOverviewView(LoginRequiredMixin, DetailView):
-    model = Organization
+
+class OrganizationOverviewView(LoginRequiredMixin, OrganizationDetailView):
     template_name = 'management/organization_details.html'
-
-    def get_context_data(self, **kwargs: Any) -> Any:
-        context = super().get_context_data(**kwargs)
-
-        context['field_names'] = {
-            'name': 'Nome',
-            'website': 'Site',
-            'get_principals': 'Responsável(eis)',
-            'created_by': 'Criado por',
-            'created_at': 'Criado em',
-            'updated_at': 'Atualizado em' 
-        }
-
-        context['model_description'] = 'da organização'
-
-        return context
--- src/organization/views/organization/organization_detail_view.py
+++ src/organization/views/organization/organization_detail_view.py
@@ -2,10 +2,6 @@ from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
 
-from django.core.paginator import PageNotAnInteger
-from django.core.paginator import Paginator
-from django.core.paginator import EmptyPage
-
 from src.organization.models.organization import Organization
 
 class OrganizationDetailView(LoginRequiredMixin, DetailView):
@@ -30,30 +26,5 @@ class OrganizationDetailView(LoginRequiredMixin, DetailView):
         }
 
         context['model_description'] = 'da organização'
-
-        context['title'] = 'Lista de organizações'
-        context['headers'] = ['Nome', 'Site', 'Responsável(eis)', 'Ações']
-        context['acessors'] = ['name', 'website', 'get_principals', 'action']
-        context['object_list'] = self.object.memberships.order_by('role')
-        context['model_name'] = 'organization'
-        context['actions'] = {
-            'organization:details': 'fa-regular fa-eye',
-            # 'organization:update': 'fa-regular fa-pen-to-square',
-            # 'organization:delete': 'fa-solid fa-delete-left'
-        }
-
-        search = self.request.GET.get('search', '')
-        if search:
-            context['search'] = search
-
-        paginator = Paginator(self.object_list, self.paginate_by)
-        page = self.request.GET.get('page')
-
-        try:
-            context['pagination_controls'] = paginator.page(page)
-        except PageNotAnInteger:
-            context['pagination_controls'] = paginator.page(1)
-        except EmptyPage:
-            context['pagination_controls'] = paginator.page(paginator.num_pages)
 
         return context
