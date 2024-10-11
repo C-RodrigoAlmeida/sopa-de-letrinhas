c3133c6bb07212cacc1edd16b5726fadc5cc32b1
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-10-11 04:15:53 -0300

    minor adjustments to the organization_overview: removed deprecated stuff

--- src/organization/views/management/organization_overview.py
+++ src/organization/views/management/organization_overview.py
@@ -21,18 +21,5 @@ class OrganizationOverviewView(TemplateView):
         membership_list_view.setup(request=self.request, *self.args, **self.kwargs)
         membership_list_view.object_list = membership_list_view.get_queryset()
 
-        search = self.request.GET.get('search', '')
-        not_members = Membership.objects.filter(
-            organization_id=self.kwargs.get('pk', None),
-            approved=False,
-            deleted_at__isnull=True,
-            user__first_name__icontains=search,
-            user__last_name__icontains=search,
-            user__email__icontains=search,
-            user__is_active=True
-        ).select_related('user', 'organization').order_by('role', 'user__first_name', 'user__last_name')
-        
 
-        return org_detail_view.get_context_data(**kwargs) | membership_list_view.get_context_data(**kwargs) | {
-            'not_members': not_members
-        }
+        return org_detail_view.get_context_data(**kwargs) | membership_list_view.get_context_data(**kwargs)
